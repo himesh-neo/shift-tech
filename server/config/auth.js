3b963d531ed52e7bf7368a4fb100ae22049104e2
Sagarsingh Jadhav <sagarsinghj@gmail.com>
2017-03-02 13:48:37 +0400

    FB callback url fix

--- server/config/auth.js
+++ server/config/auth.js
@@ -4,13 +4,13 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '265226530572106', // your App ID
         'clientSecret'  : '6203a21710bc37ee8523568bbb6abb9b', // your App Secret
-        'callbackURL'   : ' https://connected-back.herokuapp.com/auth/facebook/callback'
+        'callbackURL'   : 'http://connected-back.herokuapp.com/auth/facebook/callback'
     },
 
     'twitterAuth' : {
         'consumerKey'       : 'i5dsdrVLpDy90hvlnJg4uj2ke',
         'consumerSecret'    : 'UhI89YHxEOzkmlLSpZ7owYcGS7Ob18M7Og2Yd0CNZwKJ64Y36H',
-        'callbackURL'       : 'http://connected-back.herokuapp.com/auth/twitter/callback'
+        'callbackURL'       : 'http://localhost:3000/auth/twitter/callback'
     },
 
     'googleAuth' : {
